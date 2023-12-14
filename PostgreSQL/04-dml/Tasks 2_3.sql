18af34023c4143b84ac23bf89dcb46ce2c076d50
Denis Taziev <taziev.da@gmail.com>
2023-12-14 21:33:24 +0500

    Changed answers of task 2 and 3

--- PostgreSQL/04-dml/Tasks 2_3.sql
+++ PostgreSQL/04-dml/Tasks 2_3.sql
@@ -5,30 +5,32 @@ SELECT name FROM track
 WHERE duration >= 3.5*60;
 
 SELECT name FROM collection
-WHERE publish_date BETWEEN '2017-12-31' AND '2020-12-31';
+WHERE publish_date BETWEEN '2018-01-01' AND '2020-12-31';
 
 SELECT name FROM artist
 WHERE name NOT LIKE '% %';
 
 SELECT name FROM track
-WHERE name LIKE '% My %';
+WHERE string_to_array(lower(name), ' ') && ARRAY['my'];
 
 SELECT count(artist_id), genre_id FROM artistgenre
 GROUP BY genre_id;
 
-SELECT a.name album_name, count(t.name) count_of_tracks FROM track t
-JOIN album a ON t.album_id = a.album_id
-GROUP BY a.publish_date, a.name
-HAVING a.publish_date BETWEEN '2018-12-31' AND '2021-01-01';
+SELECT count(t.track_id) FROM track t
+JOIN album a ON t.album_id  = a.album_id
+WHERE a.publish_date BETWEEN '2019-01-01' AND '2020-12-31';
 
 SELECT a.name album_name, AVG(t.duration) FROM track t
 JOIN album a ON a.album_id = t.album_id
 GROUP BY album_name;
 
 SELECT art.name artist_name FROM artist art
-JOIN artistalbum ar_al ON art.artist_id = ar_al.artist_id
-JOIN album a ON a.album_id = ar_al.album_id
-WHERE a.publish_date NOT BETWEEN '2019-12-31' AND '2021-01-01';
+WHERE art.name NOT IN (
+	SELECT a.name FROM artist a
+	JOIN artistalbum aa ON a.artist_id = aa.artist_id
+	JOIN album al ON aa.album_id = al.album_id
+	WHERE al.publish_date BETWEEN '2020-01-01' AND '2020-12-31'
+);
 
 SELECT DISTINCT c.name FROM collection c
 JOIN collectiontrack ct ON c.collection_id = ct.collection_id
